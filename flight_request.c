e56292c2d83c5af6c6ee34aa82731036dc0ecb82
syszhpe <syszhpe@gmail.com>
2014-08-17 11:44:22 +0800

    chang

--- flight_request.c
+++ flight_request.c
@@ -28,7 +28,7 @@
 #include "php_flight.h"
 #include "flight_request.h"
 
- 
+
 
 
 zend_class_entry *flight_request_ce;
@@ -102,6 +102,122 @@ zval * flight_request_query(uint type, char * name, uint len TSRMLS_DC) {
 
 
 
+int flight_request_set_base_uri(flight_request_t *request, char *base_uri, char *request_uri TSRMLS_DC){
+    char *basename = NULL;
+    uint basename_len = 0;
+    zval *container = NULL; 
+    if (!base_uri) {
+        zval    *script_filename;
+        char    *file_name, *ext = "php";
+        size_t  file_name_len;
+        uint    ext_len;
+
+        ext_len = strlen(ext);  
+        script_filename = flight_request_query(FLIGHT_GLOBAL_VARS_SERVER, ZEND_STRL("SCRIPT_FILENAME") TSRMLS_CC);
+        do {
+            if (script_filename && IS_STRING == Z_TYPE_P(script_filename)) { 
+                zval *script_name, *phpself_name, *orig_name;
+                script_name = flight_request_query(FLIGHT_GLOBAL_VARS_SERVER, ZEND_STRL("SCRIPT_NAME") TSRMLS_CC);
+                php_basename(Z_STRVAL_P(script_filename), Z_STRLEN_P(script_filename), ext, ext_len, &file_name, &file_name_len TSRMLS_CC);
+                if (script_name && IS_STRING == Z_TYPE_P(script_name)){
+                    char    *script;
+                    size_t  script_len;
+
+                    php_basename(Z_STRVAL_P(script_name), Z_STRLEN_P(script_name),
+                            NULL, 0, &script, &script_len TSRMLS_CC);
+
+                    if (strncmp(file_name, script, file_name_len) == 0) {
+                        basename    = Z_STRVAL_P(script_name);
+                        basename_len    = Z_STRLEN_P(script_name);
+                        container   = script_name;
+                        efree(file_name);
+                        efree(script);
+                        break;
+                    }
+                    efree(script);  
+                }
+                zval_ptr_dtor(&script_name);
+                phpself_name = flight_request_query(FLIGHT_GLOBAL_VARS_SERVER, ZEND_STRL("PHP_SELF") TSRMLS_CC);
+                if (phpself_name && IS_STRING == Z_TYPE_P(phpself_name)) {
+                    char    *phpself;
+                    size_t  phpself_len;
+
+                    php_basename(Z_STRVAL_P(phpself_name), Z_STRLEN_P(phpself_name), NULL, 0, &phpself, &phpself_len TSRMLS_CC);
+                    if (strncmp(file_name, phpself, file_name_len) == 0) {
+                        basename     = Z_STRVAL_P(phpself_name);
+                        basename_len = Z_STRLEN_P(phpself_name);
+                        container = phpself_name;
+                        efree(file_name);
+                        efree(phpself);
+                        break;
+                    }
+                    efree(phpself);
+                }
+                zval_ptr_dtor(&phpself_name);
+                orig_name = flight_request_query(FLIGHT_GLOBAL_VARS_SERVER, ZEND_STRL("ORIG_SCRIPT_NAME") TSRMLS_CC);
+                if (orig_name && IS_STRING == Z_TYPE_P(orig_name)) {
+                    char    *orig;
+                    size_t  orig_len;
+                    php_basename(Z_STRVAL_P(orig_name), Z_STRLEN_P(orig_name), NULL, 0, &orig, &orig_len TSRMLS_CC);
+                    if (strncmp(file_name, orig, file_name_len) == 0) {
+                        basename     = Z_STRVAL_P(orig_name);
+                        basename_len = Z_STRLEN_P(orig_name);
+                        container = orig_name;
+                        efree(file_name);
+                        efree(orig);
+                        break;
+                    }
+                    efree(orig);
+                }
+                zval_ptr_dtor(&orig_name);
+                efree(file_name);         
+            }
+        }while(0);
+        zval_ptr_dtor(&script_filename);
+        if (basename && strstr(request_uri, basename) == request_uri) {
+            if (*(basename + basename_len - 1) == '/') {
+                --basename_len;
+            } 
+            zend_update_property_stringl(flight_request_ce, request, ZEND_STRL(FLIGHT_REQUEST_PROPERTY_NAME_BASE), basename,
+                    basename_len TSRMLS_CC);
+            if (container) {
+                zval_ptr_dtor(&container);
+            }
+            return 1; 
+        }else if(basename){
+            size_t  dir_len;
+            char    *dir = estrndup(basename, basename_len); /* php_dirname might alter the string */
+
+            dir_len = php_dirname(dir, basename_len);
+            if (*(basename + dir_len - 1) == '/') {
+                --dir_len;
+            }
+            if (dir_len) {
+                if (strstr(request_uri, dir) == request_uri) {
+                    zend_update_property_string(flight_request_ce, request, ZEND_STRL(FLIGHT_REQUEST_PROPERTY_NAME_BASE), dir  TSRMLS_CC);
+                    efree(dir);
+                    if (container) {
+                        zval_ptr_dtor(&container);
+                    }
+                    return 1;
+                }
+            }
+            efree(dir); 
+        } 
+
+        if (container) {
+            zval_ptr_dtor(&container);
+        }
+        zend_update_property_string(flight_request_ce, request, ZEND_STRL(FLIGHT_REQUEST_PROPERTY_NAME_BASE), "" TSRMLS_CC);
+        return 1;  
+    }else{
+        zend_update_property_string(flight_request_ce, request, ZEND_STRL(FLIGHT_REQUEST_PROPERTY_NAME_BASE), base_uri TSRMLS_CC);
+        return 1; 
+    } 
+} 
+
+
+
 /** {{{ flight_request_t * flight_request_instance(flight_request_t *this_ptr, char *request_uri, char *base_uri TSRMLS_DC)
 */
 //创建flight_request对象，在flight_app的构造函数中调用
@@ -134,8 +250,8 @@ flight_request_t * flight_request_instance(flight_request_t *this_ptr, char *req
     }else{
         zval *uri;
         do{
-            
-//处理windows下的情况
+
+            //处理windows下的情况
 #ifdef PHP_WIN32
 
             uri = flight_request_query(FLIGHT_GLOBAL_VARS_SERVER, ZEND_STRL("HTTP_X_REWRITE_URL") TSRMLS_CC);
@@ -198,7 +314,7 @@ flight_request_t * flight_request_instance(flight_request_t *this_ptr, char *req
             //从$_SERVER['ORIG_PATH_INFO']中取出变量
             uri = flight_request_query(FLIGHT_GLOBAL_VARS_SERVER, ZEND_STRL("ORIG_PATH_INFO") TSRMLS_CC);
             if (Z_TYPE_P(uri) != IS_NULL) {
-                                settled_uri = uri;
+                settled_uri = uri;
                 break;
             }
             zval_ptr_dtor(&uri);  
@@ -220,6 +336,7 @@ flight_request_t * flight_request_instance(flight_request_t *this_ptr, char *req
 
         //设置flight_request_http的uri值
         zend_update_property(flight_request_ce, instance, ZEND_STRL(FLIGHT_REQUEST_PROPERTY_NAME_URI), settled_uri TSRMLS_CC);
+        flight_request_set_base_uri(instance, base_uri, Z_STRVAL_P(settled_uri) TSRMLS_CC);
         zval_ptr_dtor(&settled_uri);
     }
 
